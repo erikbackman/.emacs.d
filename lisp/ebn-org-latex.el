4197c7becdf4f2240a284290f99dc66169586c08
erikbackman <erikbackman@users.noreply.github.com>
2023-01-28 17:18:55 +0100

    cleanup org-latex config

--- lisp/ebn-org-latex.el
+++ lisp/ebn-org-latex.el
@@ -1,7 +1,6 @@
 ;;; ebn-org-latex.el --- Org export settings  -*- lexical-binding: t; -*-
 
 ;;; Commentary:
-;; ASdasad
 
 ;;; Code:
 
@@ -12,42 +11,7 @@
   (unless (boundp 'org-latex-classes)
     (setq org-latex-classes nil))
 
-(add-to-list 'org-latex-classes
-             '("ethz"
-               "\\documentclass[a4paper,11pt,titlepage]{memoir}
-    \\usepackage[utf8]{inputenc}
-    \\usepackage[T1]{fontenc}
-    \\usepackage{fixltx2e}
-    \\usepackage{graphicx}
-    \\usepackage{longtable}
-    \\usepackage{float}
-    \\usepackage{wrapfig}
-    \\usepackage{rotating}
-    \\usepackage[normalem]{ulem}
-    \\usepackage{amsmath}
-    \\usepackage{textcomp}
-    \\usepackage{marvosym}
-    \\usepackage{wasysym}
-    \\usepackage{amssymb}
-    \\usepackage{hyperref}
-    \\usepackage{mathpazo}
-    \\usepackage{color}
-    \\usepackage{enumerate}
-    \\definecolor{bg}{rgb}{0.95,0.95,0.95}
-    \\tolerance=1000
-          [NO-DEFAULT-PACKAGES]
-          [PACKAGES]
-          [EXTRA]
-    \\linespread{1.1}
-    \\hypersetup{pdfborder=0 0 0}"
-               ("\\chapter{%s}" . "\\chapter*{%s}")
-               ("\\section{%s}" . "\\section*{%s}")
-               ("\\subsection{%s}" . "\\subsection*{%s}")
-               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")
-               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-
-(add-to-list 'org-latex-classes
+  (add-to-list 'org-latex-classes
              '("article"
                "\\documentclass[11pt,a4paper]{article}
     \\usepackage[utf8]{inputenc}
@@ -78,20 +42,7 @@
                ("\\section{%s}" . "\\section*{%s}")
                ("\\subsection{%s}" . "\\subsection*{%s}")
                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")))
-
-(add-to-list 'org-latex-classes '("ebook"
-                                  "\\documentclass[11pt, oneside]{memoir}
-    \\setstocksize{9in}{6in}
-    \\settrimmedsize{\\stockheight}{\\stockwidth}{*}
-    \\setlrmarginsandblock{2cm}{2cm}{*} % Left and right margin
-    \\setulmarginsandblock{2cm}{2cm}{*} % Upper and lower margin
-    \\checkandfixthelayout
-    % Much more laTeX code omitted
-    "
-                                  ("\\chapter{%s}" . "\\chapter*{%s}")
-                                  ("\\section{%s}" . "\\section*{%s}")
-                                  ("\\subsection{%s}" . "\\subsection*{%s}"))))
+               ("\\paragraph{%s}" . "\\paragraph*{%s}"))))
 
 (provide 'ebn-org-latex)
 ;;; ebn-org-latex.el ends here
