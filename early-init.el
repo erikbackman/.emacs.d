58491ce1eaff508e228a0d32a6d3da67f0a89602
erikbackman <erikbackman@users.noreply.github.com>
2023-02-19 01:37:09 +0100

    cleanup

--- early-init.el
+++ early-init.el
@@ -21,9 +21,8 @@
      native-comp-deferred-compilation nil)
 
 ;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
-;(push '(font . "Sarasa Mono CL-12") default-frame-alist)
-;(push '(font . "JuliaMono-12") default-frame-alist)
-(push '(font . "JuliaMono-10.5") default-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+
+(push '(font . "Iosevka-11") default-frame-alist)
