28a9964e8d520a9563d098caa11a4e2032eee360
erikbackman <erikbackman@users.noreply.github.com>
2023-11-05 17:41:41 +0100

    stuff

--- early-init.el
+++ early-init.el
@@ -1,10 +1,3 @@
-;; (add-to-list 'default-frame-alist '(height . 1200))
-;; (add-to-list 'default-frame-alist '(width . 2500))
-
-;(push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-
-;;(push '(font . "Fantasque Sans Mono-14") default-frame-alist)
-;;(push '(font . "M PLUS 1 Code-14") default-frame-alist)
-(push '(font . "DejaVu Sans Mono-12") default-frame-alist)
+(push '(font . "Rec Mono Duotone-12") default-frame-alist)
