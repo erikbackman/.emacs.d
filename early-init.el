5433a291b14f8c1b4501fc306efce228ab344c10
erikbackman <erikbackman@users.noreply.github.com>
2024-02-12 02:13:23 +0100

    change font

--- early-init.el
+++ early-init.el
@@ -1,5 +1,9 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-(push '(font . "Rec Mono Duotone-12") default-frame-alist)
+;;(push '(font . "Rec Mono Duotone-12") default-frame-alist)
+(push '(font . "Iosevka-14") default-frame-alist)
+(setq inhibit-startup-screen t)
+(setq initial-buffer-choice nil)
+
 
